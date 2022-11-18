91ee25c801108924692e988549fc82bc1d914068
runtia <88037966+runtia@users.noreply.github.com>
2022-11-18 12:13:13 -0500

    Update popular-movies-nice-dataframe.py

--- popular-movies-nice-dataframe.py
+++ popular-movies-nice-dataframe.py
@@ -13,7 +13,7 @@ import codecs
 def loadMovieNames():
     movieNames = {}
     # CHANGE THIS TO THE PATH TO YOUR u.ITEM FILE:
-    with codecs.open("E:/SparkCourse/ml-100k/u.ITEM", "r", encoding='ISO-8859-1', errors='ignore') as f:
+    with codecs.open("D:/SparkCodes/ml-100k/u.ITEM", "r", encoding='ISO-8859-1', errors='ignore') as f:
         for line in f:
             fields = line.split('|')
             movieNames[int(fields[0])] = fields[1]
@@ -31,7 +31,7 @@ schema = StructType([ \
                      StructField("timestamp", LongType(), True)])
 
 # Load up movie data as dataframe
-moviesDF = spark.read.option("sep", "\t").schema(schema).csv("file:///SparkCourse/ml-100k/u.data")
+moviesDF = spark.read.option("sep", "\t").schema(schema).csv("file:///SparkCodes/ml-100k/u.data")
 
 movieCounts = moviesDF.groupBy("movieID").count()
 
